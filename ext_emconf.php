046231e3e43625989201efabf8289a3dcc521fb6
Stefan Weil <sw@weilnetz.de>
2024-10-24 14:36:15 +0200

    Update ext_emconf.php to support TYPO3 12
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '4.0.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '10.4.99-11.5.99',
+			'typo3' => '10.4.99-12.4.99',
 		),
 	),
 );
